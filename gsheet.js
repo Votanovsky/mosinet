12dd86f4eaea2d509d37c9eeee5d3680d7c4a1d7
Mikich Esaian <woknsushi@yandex.ru>
2023-05-25 00:30:10 +0300

    add telegram bot functionality

--- gsheet.js
+++ gsheet.js
@@ -4,7 +4,7 @@ $(function() {
 
       // Ссылка, которую получили на этапе публикации приложения
       let appLink = "https://script.google.com/macros/s/AKfycby8LDGjGbzooax8v5Ge9una3rLHAEczUo7MpGSZNX7fP5QV-V8ur1XIwJQON35gCLigLw/exec";
-   
+      
       // Сообщение при успешной отправке данных
       let successRespond = 'Заявка принята! Наш менеджер свяжется с вами в ближайшее время.';
    
@@ -27,13 +27,21 @@ $(function() {
    
       // Кнопка отправки формы
       let submitButton = $(this).parent().find('.modal_t') ? $(this).find('.form-submit') : $('.btn_check');
-   
-   
+      // Сообщение, отправляемое на закрытый канал в телеграмме в случае, если пользователь успешно отправляет контактные данные
+      let telegramText =(form.id==="form1"?"Клиент ожидает обратной связи!":"Клиент хочет проверить возможность подключения!")+"\n---------------------------------------------"
+      
       // FormData
       let fd = new FormData(form);
-   
+      for(const [key,value] of fd.entries())
+        telegramText+=`\n${key}: ${value}` 
+      let
+        telegramBotAPIKey = '5921150643:AAFRXdcLnRz_eJRwdlnrdK5BlzU67i6lnLA',
+        telegramAPIURL = `https://api.telegram.org/bot${telegramBotAPIKey}/sendMessage`
+        telegramBotRequestBody = {
+          chat_id:-1001874196244,
+          text:telegramText
+        }
       $.ajax({
-   
         url: appLink,
         type: "POST",
         data: fd,
@@ -69,11 +77,15 @@ $(function() {
     //   formRespond.html(successRespond).css('color', '#37b599');
        
       // Возвращаем активность кнопке отправки
-      submitButton.prop('disabled', false);
+        submitButton.prop('disabled', false);
    
         // Очищаем поля формы
         form.reset();
-   
+        fetch(telegramAPIURL,{
+          method:"POST",
+          headers:{'Content-Type':"application/json"},
+          body:JSON.stringify(telegramBotRequestBody)
+        })
       } else {
         formRespond.html(errorRespond);
 
